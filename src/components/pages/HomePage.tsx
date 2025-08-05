9ba509a260e9be8790237438d9d87c690e3912f1
vuraweg <virat52418@gmail.com>
2025-08-05 21:18:57 +0530

    Updated HomePage.tsx

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -345,7 +345,7 @@ export const HomePage: React.FC<HomePageProps> = ({
       )}
 
       {/* Additional Features Teaser */}
-      <div className="bg-gradient-to-r from-grey to-blue-900 text-white py-16 px-4 sm:px-0">
+      <div className="bg-gradient-to-r from-grey-500 to-blue-900 text-white py-16 px-4 sm:px-0">
         <div className="container-responsive text-left">
           <div className="max-w-3xl mx-auto">
             <h3 className="text-2xl sm:text-3xl font-bold mb-4 text-pink-500">
